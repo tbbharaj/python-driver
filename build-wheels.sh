4f49fbb8fcd584a7c0ff764fe347e7e966f66fe5
Tanveen Bharaj <tbbharaj@amazon.com>
2021-06-29 17:28:53 -0500

    final changes 1

--- build-wheels.sh
+++ build-wheels.sh
@@ -16,12 +16,12 @@ done
 
 
 # Create binary wheels
-/opt/python/${PYTHON}/bin/python3 setup.py bdist_wheel
+#/opt/python/${PYTHON}/bin/python3 setup.py bdist_wheel
 
 # Normalize resulting binaries to a common format
-for whl in dist/*.whl; do
-    auditwheel repair $whl --plat "${PLAT}" -w wheelhouse
-done
+#for whl in dist/*.whl; do
+#    auditwheel repair $whl --plat "${PLAT}" -w wheelhouse
+#done
 
 if [ `uname -m` = 'aarch64' ]; then
 	/opt/python/${PYTHON}/bin/pip3 install /io/*-manylinux2014_aarch64.whl
